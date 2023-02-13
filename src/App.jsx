356068b3ae14c9b2414fa3eb5970c49dc21e2cb4
CryptoFallen <richardtom1203@protonmail.com>
2023-02-13 13:32:27 +0000

    SASS CSS added to help style

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
+import "./categories.styles.scss";
+
 function App() {
   const categories = [
     {
       id: 1,
       title: "Hats",
     },
+    {
+      id: 1,
+      title: "Pants",
+    },
+    {
+      id: 1,
+      title: "Dress",
+    },
   ];
 
   return (
     <div className="categories-container">
-      {categories.map(({ title }) => (
-        <div className="category-container">
+      {categories.map(({ title, id }) => (
+        <div key={id} className="category-container">
           <div className="background-image" />
           <div className="category-body-container">
             <h2>{title}</h2>
