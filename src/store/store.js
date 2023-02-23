621666a230b5f6da6abae7ac2c6e9329a25d5157
CryptoFallen <richardtom1203@protonmail.com>
2023-02-23 19:51:27 +0000

    redux-persist installed and used to allow  for user values to be saved  inside of local storage

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,42 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
-// import { loggerMiddleware as logger } from './middlewares/logger';
+
+// persist used to save the state into local storage.
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+// import logger from 'redux-logger';
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
-  Boolean
-);
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log("type: ", action.type);
+  console.log("payload: ", action.payload);
+  console.log("currentState: ", store.getState());
+
+  next(action);
+
+  console.log("next state: ", store.getState());
+};
+
+const middleWares = [loggerMiddleware];
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
